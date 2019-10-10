64576366bbff6ee1fae2e5704047359e9702f50e
Ivan Zhitnikov <zhitnikov.vanya@gmail.com>
2019-10-10 23:14:43 +0300

    task 344

--- leet_344.py
+++ leet_344.py
@@ -1,3 +1,6 @@
+# https://leetcode.com/problems/reverse-string/
+# Runtime: 224 ms, faster than 93.14% of Python3 online submissions for Reverse String.
+
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         """
